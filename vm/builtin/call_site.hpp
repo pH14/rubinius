4dc860e9987b34e94a8b1344dae7a5d3037141f4
Paul Hemberger <pH14@mit.edu>
2014-03-17 12:35:17 -0400

    entry hook w/ blocks working

--- vm/builtin/call_site.hpp
+++ vm/builtin/call_site.hpp
@@ -2,12 +2,16 @@
 #define RBX_BUILTIN_CALL_SITE_HPP
 
 #include "builtin/object.hpp"
+#include "builtin/symbol.hpp"
 #include "machine_code.hpp"
 #include "object_utils.hpp"
+#include "arguments.hpp"
 
 namespace rubinius {
 
   class Dispatch;
+  class Arguments;
+  class Symbol;
 
   typedef Object* (CacheExecuteFunc)(STATE, CallSite*, CallFrame*, Arguments&);
   typedef Object* (FallbackExecuteFunc)(STATE, CallSite*, CallFrame*, Arguments&);
@@ -105,6 +109,60 @@ namespace rubinius {
     static bool lookup_method_missing(STATE, CallFrame* call_frame, Arguments& args, Dispatch& dis, Object* self, Module* begin);
 
     Object* execute(STATE, CallFrame* call_frame, Arguments& args) {
+      Object* const recv = args.recv();
+
+      if(recv->is_secure_context_p() && args.total() > 0) {
+        std::cerr << "[vm/CallSite#execute] Receiver has a secure context for f'n: " << args.name()->cpp_str(state) << "\n";
+        std::cerr << "[vm/CallSite#execute] Number of args before call is: " << args.total() << "\n";
+
+        args.set_recv(recv->get_secure_context_prim(state));
+
+        Object* hooked_args = (*executor_)(state, this, call_frame, args);
+
+        if(!hooked_args) {
+          std::cerr << "[vm/CallSite#execute] Secure context didn't return anything.\n";
+          std::cerr << "[vm/CallSite#execute] Number of args is: " << args.total() << "\n";
+
+          for(size_t i = 0; i < args.as_array(state)->size(); i++) {
+            std::cerr << "[vm/CallSite#execute] Arg << " << i << " is " << args.as_array(state)->get(state, i)->to_string(state) << "\n";
+          }
+
+          // If the secure context doesn't return anything, args will be prepended with the symbol of the method call
+          // so we remove that and call the function on the original receiver
+          args.shift(state);
+          args.set_recv(recv);
+        } else {
+          Object* const original_recv = recv;
+          Array* ary = Array::create(state, args.total());
+          Array* each_hooked_arg = try_as<Array>(hooked_args);
+
+          std::cerr << "[vm/CallSite#execute] Number of hooked args is: " << each_hooked_arg->size() << "\n";
+
+          size_t args_range = each_hooked_arg->size();
+          bool block_given = args.block() != NULL;
+
+          if (!block_given) {
+            args_range -= 1;
+          }
+
+          for(size_t i = 0; i < args_range; i++) {
+            std::cerr << "[vm/CallSite#execute] Hooked arg is " << each_hooked_arg->get(state,i)->to_string(state) << "\n";
+            ary->set(state, i, each_hooked_arg->get(state, i));
+          }
+
+          Arguments updated_args(args.name(), ary);
+          updated_args.set_recv(original_recv);
+
+          if (block_given) {
+            std::cerr << "[vm/CallSite#execute] Original args does have block. Setting it on new args.\n";
+            updated_args.set_block(each_hooked_arg->get(state, each_hooked_arg->size() - 1));
+          }
+
+          std::cerr << "[vm/CallSite#execute] Now calling original.\n";
+          return (*executor_)(state, this, call_frame, updated_args);
+        }
+      }
+
       return (*executor_)(state, this, call_frame, args);
     }
 
